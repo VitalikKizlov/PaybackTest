8863480543976db88cfe203297a0f7477f93d48b
Vitalii Kizlov <vitalii kizlov>
2023-10-24 14:03:29 +0200

    Initial Commit

---
+++ PaybackTest/PaybackTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaybackTestApp.swift
+//  PaybackTest
+//
+//  Created by Vitalii Kizlov on 24.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PaybackTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
